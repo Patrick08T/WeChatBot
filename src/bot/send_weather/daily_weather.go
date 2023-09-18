46bd86b1c9bc454cef2b0e5b57557a03185aa921
patrick <506765973@qq.com>
2023-09-18 23:32:03 +0800

    [mod] weather ticker

--- src/bot/send_weather/daily_weather.go
+++ src/bot/send_weather/daily_weather.go
@@ -8,7 +8,7 @@ import (
 
 func DailyWeatherToFriend(self *openwechat.Self, friends openwechat.Friends, name string) {
 	fs := friends.Search(1, func(friend *openwechat.Friend) bool { return friend.NickName == name })
-	tick.DailyTicker(22, 36, 0, func() {
+	tick.DailyTicker(9, 0, 0, func() {
 		resp := weather.GetTodayWeather()
 		if resp != nil {
 			for _, friend := range fs {
